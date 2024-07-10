f12fc2ccbc7625c880522f231e04f61699522c62
Nikita Naumov <46nik46@gmail.com>
2024-07-10 03:02:42 +0300

    main

---
+++ ASPNETCoreForDockerApp/Models/ErrorViewModel.cs
@@ -0,0 +1,11 @@
+using System;
+
+namespace ASPNETCoreForDockerApp.Models
+{
+    public class ErrorViewModel
+    {
+        public string RequestId { get; set; }
+
+        public bool ShowRequestId => !string.IsNullOrEmpty(RequestId);
+    }
+}
